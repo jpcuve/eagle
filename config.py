be88e1c1744251d685a05c38e3243d7f2c32279d
Jean-Pierre Cuvelliez <jpcuvelliez@gmail.com>
2022-09-26 11:59:43 +0200

    .

---
+++ config.py
@@ -0,0 +1,3 @@
+from pathlib import Path
+
+PATH_TO_GCP_CREDENTIALS = Path.home() / 'eagle-363706-046dc4dba4ca.json'
--- main.py
+++ main.py
@@ -1,8 +1,40 @@
-from flask import Flask
+import os
+from pathlib import Path
+
+from flask import Flask, current_app, redirect, url_for, render_template
+
+CONFIGURATION_LOCATION = 'FLASK_CONFIG'
+
+
+def load_my_model():
+    with open(Path('.') / 'dummy_model.txt') as f:
+        return f.read()
+
 
 app = Flask(__name__)
+app.config.from_object('config')  # read from config.py into app.config
+if CONFIGURATION_LOCATION in os.environ.keys():  # if FLASK_CONFIG defined in environment
+    app.config.from_envvar(CONFIGURATION_LOCATION)  # then overwrite app.config from that file
+if app.config['ENV'] == 'development':  # if working from my pc
+    # point GOOGLE_APPLICATION_CREDENTIALS to my json credentials file
+    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = str(app.config['PATH_TO_GCP_CREDENTIALS'])
+# at this point I can attach any variable to app so that it is available anywhere: app.key = value
+# for instance, my ML model
+app.model = load_my_model()
+print(app.model)
 
 
-@app.route("/")
+@app.route('/')
+def home():
+    # redirection de l'url principal vers la page d'accueil
+    return render_template('index.html')
+
+@app.route('/favicon.ico')
+def favicon():
+    return redirect(url_for('static', filename='favicon.ico'))
+
+
+@app.route("/hello-world")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return current_app.model  # loaded from dummy_model.txt
+
