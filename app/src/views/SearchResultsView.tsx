67a34dd97d888af413ec06071b2f646a1e635943
jpcuve <jpcuvelliez@gmail.com>
2022-09-27 09:04:59 +0200

    .

--- app/src/views/SearchResultsView.tsx
+++ app/src/views/SearchResultsView.tsx
@@ -2,15 +2,23 @@ import {FC} from 'react'
 import {useSelector} from 'react-redux'
 import {ApplicationState} from '../store'
 import {SearchResult} from '../domain/searchResult'
+import {Grid} from "@mui/material";
 
 const SearchResultsView: FC = () => {
   const searchResults = useSelector<ApplicationState, SearchResult[]>(state => state.searchResults)
   return (
-    <ul>
-      {searchResults.map(it => (
-        <li key={it.filename}>{it.filename}</li>
-      ))}
-    </ul>
+      <Grid container>
+        <Grid item xs={12}>
+          Search results
+        </Grid>
+        <Grid item xs={12}>
+          <ul>
+            {searchResults.map(it => (
+              <li key={it.filename}>{it.filename}</li>
+            ))}
+          </ul>
+        </Grid>
+      </Grid>
   )
 }
 
